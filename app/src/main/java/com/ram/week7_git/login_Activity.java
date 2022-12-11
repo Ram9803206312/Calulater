95e02c7e03c85668dd3b49c23768f3b6c41e2f98
Ram Nepali <pariyarram056@gmail.com>
2022-12-11 10:46:39 +0545

    added login activity

---
+++ app/src/main/java/com/ram/week7_git/login_Activity.java
@@ -0,0 +1,14 @@
+package com.ram.week7_git;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
